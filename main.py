c66a848290c8b45cae0aea7c8e526e7d68c94eab
Nick LaMonica <nila2238@lasp.colorado.edu>
2020-11-10 15:57:57 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+
+win = pygame.display.set_mode((500, 500))
+
+pygame.display.set_caption("Rootin Tootin Yeehaw Simulator")
+
+run = True
+
+while run:
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.display.update()
+
+pygame.quit()
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Rootin Tootin Yeehaw Simulator
+### Bryce Woods, Jaclyn Stickrod, Nick LaMonica
+
+You'll need Python and pygame installed!
+
