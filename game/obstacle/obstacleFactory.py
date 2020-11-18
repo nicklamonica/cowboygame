0dc761d3849df9cbcd262fab0b29d82021c8b1c8
Jaclyn Stickrod <mail4jaci@gmail.com>
2020-11-17 17:01:56 -0700

    Maybe fixed imports

--- game/obstacle/obstacleFactory.py
+++ game/obstacle/obstacleFactory.py
@@ -1,18 +1,9 @@
-from .bird import Bird
-from .cactus import Cactus
-from .rock import Rock
-
+from obstacle import Obstacle
 import random
 
 class ObstacleFactory:
     def createObstacles(self):
         obstacles = []
         for i in range(1, 10):
-            rand = random.randint(0, 2)
-            if rand == 0:
-                obstacles.append(Cactus(random.randint(100, 500) + (100*i), 385))
-            elif rand == 1:
-                obstacles.append(Bird(random.randint(100, 500) + (100*i), 355))
-            else:
-                obstacles.append(Rock(random.randint(100, 500) + (100*i), 440))
+            obstacles.append(Obstacle(random.randint(100, 500) + (100*i), 385))
         return obstacles
